c5b36a6805be3e3814e4a1859cd542303408ab79
Jared Allen <jarmallen@gmail.com>
2016-05-05 10:39:59 -0700

    Commit again

--- Cat Years/ViewController.swift
+++ Cat Years/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var catAgeTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findAge(sender: AnyObject) {
+        
+        var catAge: Int = Int(catAgeTextField.text!)!
+        catAge = catAge * 7
+        resultLabel.text = "Your cat is \(catAge) in cat years"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
